816c92af9b15e0889e0b8c7cad2caffb3aa221a1
OfekBar <75580980+OfekBar@users.noreply.github.com>
2021-01-02 17:44:56 +0200

    Add files via upload

---
+++ DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/movie_crew.sql
@@ -0,0 +1,12 @@
+CREATE  TABLE DbMysql08.movie_crew (
+  `crew_id` INT NOT NULL AUTO_INCREMENT ,
+  `profile_id` INT NOT NULL ,
+  `role` VARCHAR(50) NOT NULL ,
+  `movie_id` INT NOT NULL ,
+  PRIMARY KEY (`crew_id`) ,
+  FOREIGN KEY (movie_id) REFERENCES DbMysql08.movies(movie_id),
+  FOREIGN KEY (profile_id) REFERENCES DbMysql08.profile(profile_id),
+  FOREIGN KEY (role) REFERENCES DbMysql08.department(role),
+  UNIQUE INDEX `crew_id_UNIQUE` (`crew_id` ASC) ,
+  INDEX `role_index` (`role` ASC) 
+);
