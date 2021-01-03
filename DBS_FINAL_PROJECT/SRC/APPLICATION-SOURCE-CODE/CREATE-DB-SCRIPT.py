7f891d11ba404beb98d8af263c28002b8e461e2a
odedkris <odedkris@gmail.com>
2021-01-03 15:13:09 +0200

    front end job on home page and index

--- DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/CREATE-DB-SCRIPT.py
+++ DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/CREATE-DB-SCRIPT.py
@@ -4,15 +4,26 @@ from datetime import date
 from datetime import datetime
 import queries
 
-def connect_to_db():
+
+def connect_to_db(query,args=None):
+    """
+    connects to the database, execute the query and return an iterator.
+
+    keyword arguments:
+    query -- the query we want to execute
+    args -- additional arguments
+    """
+
+    # The Database login details
     config = {
         'user': 'DbMysql08',
         'password': 'DbMysql08',
-        'host': 'mysqlsrv1.cs.tau.ac.il',
+        'host': 'mysqlsrv1.cs.tau.ac.il',       #use 'localhost' or '127.0.0.1' if running from home
         'database': 'DbMysql08',
-        'port': 3306,
+        'port': 3306,                           #use your forward if running from home
         'raise_on_warnings': True,
     }
+
     try:
         cnx = mysql.connector.connect(**config)
     except mysql.connector.Error as err:
