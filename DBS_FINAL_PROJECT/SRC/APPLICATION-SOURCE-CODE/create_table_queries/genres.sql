50fdb6403316890004d865dbfa5bcb2917838d1d
OfekBar <75580980+OfekBar@users.noreply.github.com>
2021-01-10 01:51:51 +0200

    Update genres.sql

--- DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/genres.sql
+++ DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/genres.sql
@@ -3,5 +3,6 @@ CREATE  TABLE `DbMysql08`.`genres` (
   `movie_id` INT NOT NULL ,
   `genre` CHAR(20) NOT NULL ,
   PRIMARY KEY (`genre_id`) ,
-  FOREIGN KEY (movie_id) REFERENCES `DbMysql08`.`movies` (movie_id)
+  FOREIGN KEY (movie_id) REFERENCES `DbMysql08`.`movies` (movie_id),
+  UNIQUE INDEX `genre_id_UNIQUE` (`genre_id` ASC) 
 );
