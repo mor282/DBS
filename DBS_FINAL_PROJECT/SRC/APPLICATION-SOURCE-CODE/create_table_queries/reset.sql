9cd85babd6d4a13befc1f1e0704d62cbe25e9621
Ben Massarano <53153950+BenM61@users.noreply.github.com>
2021-01-10 17:07:35 +0200

    Update reset.sql

--- DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/reset.sql
+++ DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/reset.sql
@@ -64,7 +64,7 @@ CREATE  TABLE DbMysql08.genres (
 CREATE  TABLE DbMysql08.movie_crew (
   `crew_id` INT NOT NULL AUTO_INCREMENT ,
   `profile_id` INT NOT NULL ,
-  `role` VARCHAR(50) NOT NULL ,
+  `role` CHAR(50) NOT NULL ,
   `movie_id` INT NOT NULL ,
   PRIMARY KEY (`crew_id`) ,
   FOREIGN KEY (movie_id) REFERENCES DbMysql08.movies(movie_id),
