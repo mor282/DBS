b1443a82ed02aaa52fc5cea1e2b1484d34d60989
Ben Massarano <53153950+BenM61@users.noreply.github.com>
2021-01-10 02:02:11 +0200

    Update set.sql

--- DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/set.sql
+++ DBS_FINAL_PROJECT/SRC/APPLICATION-SOURCE-CODE/create_table_queries/set.sql
@@ -49,7 +49,7 @@ CREATE  TABLE DbMysql08.genres (
   `movie_id` INT NOT NULL ,
   `genre` CHAR(20) NOT NULL ,
   PRIMARY KEY (`genre_id`) ,
-  FOREIGN KEY (movie_id) REFERENCES `DbMysql08`.`movies` (movie_id)
+  FOREIGN KEY (movie_id) REFERENCES `DbMysql08`.`movies` (movie_id),
   UNIQUE INDEX `genre_id_UNIQUE` (`genre_id` ASC)
 );
 
